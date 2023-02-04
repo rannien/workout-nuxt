b5bdb8ee06dc172fe9d43a1399d6413baea187bb
Csaba Verebélyi <verebelyi.csaba@zengo.ez>
2023-02-04 18:44:45 +0100

    feat: add pinia

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const counter = useCounterStore();
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
+    <button @click="counter.increment()">Click me: {{ counter.count }}</button>
   </div>
 </template>
